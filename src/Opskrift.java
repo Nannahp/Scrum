208231b4d2d436163be22ada46b06031f2f12d5b
Emre <Emse0001@stud.kea.dk>
2023-09-26 14:42:07 +0200

    Src

--- src/Opskrift.java
+++ src/Opskrift.java
@@ -1,12 +1,12 @@
 public class Opskrift {
     public static void main(String[] args) {
-        /* Arrayen skal kunne løbe, indeholdende opskrift objekter, igennem.
-           Nøjes med at kunne søge på en ting først, så kan det udvides senere.
-           Det må betyde et field i klassen, som indeholder den oplysning.
-           Skal nok løses sidst, da den kræver nogle smarte designbeslutninger omkring funktionalitet og kodeafvikling.
-           Kan løses med en attribut.
-           Her vil jeg lade det være op til jeres gruppe at finde en løsning.  ;-)
-           Det betyder en klar forståelse for programafviklingen, Menu punkter og måske en Switch til at håndtere brugerens valg.
+        /* 1. Arrayen skal kunne løbe, indeholdende opskrift objekter, igennem.
+           2. Nøjes med at kunne søge på en ting først, så kan det udvides senere.
+           3. Det må betyde et field i klassen, som indeholder den oplysning.
+           4. Skal nok løses sidst, da den kræver nogle smarte designbeslutninger omkring funktionalitet og kodeafvikling.
+           5. Kan løses med en attribut.
+           6. Her vil jeg lade det være op til jeres gruppe at finde en løsning.  ;-)
+           7. Det betyder en klar forståelse for programafviklingen, Menu punkter og måske en Switch til at håndtere brugerens valg.
          */
     }
 }
